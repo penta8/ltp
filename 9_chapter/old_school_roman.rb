a140259022780b7d718e5a6f5be2b1a0e469f153
penta8 <utn_etudiant@yahoo.fr>
2017-02-22 00:57:50 -0600

    Refactoring old_school_roman to use each and constant EQUIV

--- 9_chapter/old_school_roman.rb
+++ 9_chapter/old_school_roman.rb
@@ -1,15 +1,11 @@
+EQUIV = [['M', 1000], ['D', 500], ['C', 100],
+         ['L', 50], ['X', 10], ['V', 5], ['I', 1]]
+
 def to_roman(number)
-  roman = 'M' * (number / 1000)
-  number = ((number / 1000) * 1000 - number).abs
-  roman += 'D' * (number / 500)
-  number = ((number / 500) * 500 - number).abs
-  roman += 'C' * (number / 100)
-  number = ((number / 100) * 100 - number).abs
-  roman += 'L' * (number / 50)
-  number = ((number / 50) * 50 - number).abs
-  roman += 'X' * (number / 10)
-  number = ((number / 10) * 10 - number).abs
-  roman += 'V' * (number / 5)
-  number = ((number / 5) * 5 - number).abs
-  roman += 'I' * number
+  roman = ''
+  EQUIV.each do |equiv|
+    roman += equiv[0] * (number / equiv[1])
+    number = ((number / equiv[1]) * equiv[1] - number).abs
+  end
+  roman
 end

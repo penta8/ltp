ae80550d70dae374216ae6a5049306a3b54be2aa
penta8 <utn_etudiant@yahoo.fr>
2017-02-23 23:51:38 -0600

    Returning a string instead of using puts

--- 10_chapter/4_99_bottles_of_beer.rb
+++ 10_chapter/4_99_bottles_of_beer.rb
@@ -1,18 +1,19 @@
+# Integer -> String
+
 def bottles_of_beer(bottles)
   if bottles == 0
-    puts "No more bottles of beer on the wall, no more bottles of beer."
-    puts "Go to the store and buy some more, 99 bottles of beer on the wall."
-    return nil
+    "No more bottles of beer on the wall, no more bottles of beer.\n" +
+    "Go to the store and buy some more, 99 bottles of beer on the wall."
   elsif bottles == 1
-    puts "1 bottle of beer on the wall, 1 bottle of beer."
-    puts "Take one down and pass it around, no more bottles of beer on the wall."
+    "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+    "Take one down and pass it around, no more bottles of beer on the wall.\n\n" +
     bottles_of_beer(bottles - 1)
   else
     noun = bottles > 2 ? 'bottles' : 'bottle'
-    puts bottles.to_s + " bottles of beer on the wall, " + bottles.to_s + " bottles of beer."
-    puts "Take one down and pass it around, " + (bottles - 1).to_s + " #{noun} of beer on the wall."
+    bottles.to_s + " bottles of beer on the wall, " + bottles.to_s + " bottles of beer.\n" +
+    "Take one down and pass it around, " + (bottles - 1).to_s + " #{noun} of beer on the wall.\n\n" +
     bottles_of_beer(bottles - 1)
   end
 end
 
-bottles_of_beer(99)
+puts bottles_of_beer(99)

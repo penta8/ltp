5825f468d105deea73614b1d3c84ca62f980847f
penta8 <utn_etudiant@yahoo.fr>
2017-02-24 00:20:01 -0600

    Using rubocop and refactoring code

--- 10_chapter/4_99_bottles_of_beer.rb
+++ 10_chapter/4_99_bottles_of_beer.rb
@@ -1,19 +1,23 @@
 # Integer -> String
 
 def bottles_of_beer(bottles)
-  if bottles == 0
-    "No more bottles of beer on the wall, no more bottles of beer.\n" +
-    "Go to the store and buy some more, 99 bottles of beer on the wall."
-  elsif bottles == 1
-    "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-    "Take one down and pass it around, no more bottles of beer on the wall.\n\n" +
-    bottles_of_beer(bottles - 1)
+  if bottles.zero?
+    'No more bottles of beer on the wall, no more bottles of beer.\n' \
+      'Go to the store and buy some more, 99 bottles of beer on the wall.'
   else
-    noun = bottles > 2 ? 'bottles' : 'bottle'
-    bottles.to_s + " bottles of beer on the wall, " + bottles.to_s + " bottles of beer.\n" +
-    "Take one down and pass it around, " + (bottles - 1).to_s + " #{noun} of beer on the wall.\n\n" +
-    bottles_of_beer(bottles - 1)
+    message(bottles) + "\n\n" + bottles_of_beer(bottles - 1)
   end
+end
+
+def message(bottles)
+  noun = bottles > 2 ? 'bottles' : 'bottle'
+  bottles.to_s + " #{noun} of beer on the wall, " + bottles.to_s +
+    " #{noun} of beer.\n" + 'Take one down and pass it around, ' +
+    if bottles > 1
+      (bottles - 1).to_s + " #{noun} of beer on the wall."
+    else
+      'no more bottles of beer on the wall.'
+    end
 end
 
 puts bottles_of_beer(99)
